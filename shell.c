aa34e50a4b737b35ffece269801444c7a24dc043
Xinhui <x.xu.1102@gmail.com>
2016-11-21 15:34:46 -0500

    try fork

--- shell.c
+++ shell.c
@@ -4,37 +4,40 @@
 #include <string.h>
 
 int main() {
-  char buf[50];
-  printf("what's good:\n");
-  
-  fgets(buf, sizeof(buf), stdin);
-  
-  char * bufadd = buf;
-  
-  printf("value of buf: %s\n",buf);
-  printf("address of buf: %u\n",&buf);
-  printf("value of bufadd: %u\n",bufadd);
-  printf("address of bufadd: %u\n",&bufadd);
-  
-  printf("\tstrippin newline\n");
-  bufadd = strsep(&bufadd, "\n");
-  
-  printf("value of buf: %s\n",buf);
-  printf("value of bufadd: %u\n",bufadd);
-  
-  char *command[10];
 
-  int i = 0;
-  while (bufadd) {
-    command[i] = strsep(&bufadd, " ");
-    i++;
+  while (1){
+    char buf[50];
+    printf("what's good:\n");
+  
+    fgets(buf, sizeof(buf), stdin);
+  
+    char * bufadd = buf;
+  
+    bufadd = strsep(&bufadd, "\n");
+  
+    char *command[10];
+
+    int i = 0;
+    while (bufadd) {
+      command[i] = strsep(&bufadd, " ");
+      printf("command[%d]: %s\n",i,command[i]);
+      i++;
+    }
+
+    command[i] = 0;
+  
+    printf("\texecute\n");
+    int f = fork();
+    if (f == 0) {
+      printf("\ti'm a child\n");
+      execvp( command[0], command );
+    }
+    else {
+      int status, r;
+      r = wait( &status );
+      printf("\twait returned: %d, status: %d\n", r, status);
+    }
+
+    return 0;
   }
-
-  command[i] = 0;
-  
-  
-  printf("\texecute\n");
-  execvp( command[0], command );
-
-  return 0;
 }
