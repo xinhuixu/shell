b72a312a273e6a1d9ce783b44991b35b39757274
xinhuixu <x.xu.1102@gmail.com>
2016-11-26 18:13:57 -0500

    discovered strchr

--- shell.c
+++ shell.c
@@ -5,31 +5,27 @@
 #include "func.h"
 
 int main() {
-
   
   while (1){
 
     /*
       TODO: 
-      - prompt current working dir
       - ? C^D reprints last stdout
       - use file to test instead of retyping
       - sep use ;
-      - illegal command should get error
      */
 
     char buf[50];
     prompt();
     fgets(buf, sizeof(buf), stdin);
     char * bufadd = buf;
-    bufadd = strsep(&bufadd, "\n");
+    *(strchr(bufadd, '\n')) = 0;
     char *command[10];
     int i = 0;
-    while (bufadd) {
-      command[i] = strsep(&bufadd, " ");
-      i++;
-    }
-    command[i] = 0;
+
+    while ((command[i] = strsep(&bufadd," ")))
+	   i++;
+	   
     int f = fork();
     if (f == 0) {
       int err = execvp( command[0], command );
