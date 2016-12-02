ad544403559e8ebebbbd83859870c2df7772eaee
inxurance <x.xu.1102@gmail.com>
2016-12-02 04:03:45 +0000

    get cmd before spec chr

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # shell
 
+**note**
+- assumed white spaces around '>', '|'
+ 
 **features**
 - ^C exits
 - input 'exit' exits
+- cd prints message if success
 
 **attempted but not achieved**
-- cd with empty space => cd ~
+- cd with empty space => cd ~ (bash behavior)
--- func.c
+++ func.c
@@ -3,12 +3,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
-
-char *def_str[3] = {
-  "cd",
-  ">",
-  "|"
-};
+#include <sys/stat.h>
+#include <sys/types.h>
 
 void prompt(){
   printf("what's good: ");
@@ -27,20 +23,40 @@ void pstr(char *c){
 }
 
 void bye(){
-  printf("all parties ends\n");
   exit(0);
+}
+
+void cmd_before(char *c1[], char *c[], int i){
+  // set c1 into the c before special chrs
+      int j = 0;
+      while (j < i) {
+        c1[j]=(char *)malloc(sizeof(c[j]));
+        strcpy(c1[j],c[j]);
+        j++;
+      }
+
+      /*
+      int fd = open(c[2],O_CREAT|O_RDWR,0644);
+      int r = dup2(fd,1);
+      close(fd);
+      if (r<0){
+        perror("dup2");
+        return r;
+      }*/
 }
 
 int def_check(char *c[]){
   //parray(c);
-
-  if (strcmp(c[0],def_str[0]) == 0) { //cd
+  int err;
+  int i;
+  
+  if (strcmp(c[0],"cd") == 0) { //cd
     if (c[1]){
       /*if (strcmp(c[1],"\n") == 0)
 	return chdir("~"); //bash behavior */
       int res = chdir(c[1]);
       if (res == 0)
-	printf("cd %s: success\n",c[1]);
+	      printf("cd %s: success\n",c[1]);
       return res;
     }
     else {
@@ -48,18 +64,20 @@ int def_check(char *c[]){
     }
   }
 
-  else if (c[1] && (strcmp(c[1],def_str[1]) == 0)) { //>
-    if (c[2]) {
-      int fd = open(c[2],O_CREAT|O_RDWR,0777);
-      //int din = dup(1);
-      dup2(fd,1);
-      //dup2(din,1);
+  i = 1;
+  while (c[i]) {
+  
+    if (strcmp(c[i],">") == 0){ //>
+      char *c1[i];
+      cmd_before(c1,c,i);
+      //parray(c1);
       return 0;
     }
+    //if (strcmp(c[i],def_str[2]) == 0) //<
+    //  redir_i(c,i);
+    i++;
   }
   
-  else {
-    printf("\treach end of def_check\n");
+    //printf("\treach end of def_check\n");
     return execvp(c[0],c);
-  }
 }
