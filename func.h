4ef1867de71f1bd3a39835eac4526100aea34970
xinhuixu <x.xu.1102@gmail.com>
2016-12-02 22:02:32 -0500

    add headers

--- func.h
+++ func.h
@@ -7,6 +7,7 @@ void pstr(char *);
 void bye();
 void cmd_before(char **, char **, int);
 void cmd_after(char **, char **, int);
+int is_op(char *);
 int check_status(int);
 int def_check(char *[]);
 
